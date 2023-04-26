788a2b835c7ff75694bf856740365f4cdfbb5e0d
ArtemShchuchev <shuaa@rambler.ru>
2023-04-26 21:15:15 +0700

    Add files

---
+++ drysolid/drysolid.hpp
@@ -0,0 +1,57 @@
+﻿#pragma once
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include "SecondaryFunction.h"
+
+
+class Printable
+{
+public:
+    virtual ~Printable() = default;
+
+    virtual std::string printAsHTML() const = 0;
+    virtual std::string printAsText() const = 0;
+    virtual std::string printAsJSON() const = 0;
+};
+
+class Data : public Printable
+{
+public:
+    enum class Format
+    {
+        kText,
+        kHTML,
+        kJSON
+    };
+
+    Data(std::string data, Format format)
+        : data_(std::move(data)), format_(format) {}
+
+    std::string printAsHTML() const override
+    {
+        if (format_ != Format::kHTML) {
+            throw std::runtime_error("Invalid format!");
+        }
+        return "<html>" + data_ + "<html/>";
+    }
+    std::string printAsText() const override
+    {
+        if (format_ != Format::kText) {
+            throw std::runtime_error("Invalid format!");
+        }
+        return data_;
+    }
+    std::string printAsJSON() const override
+    {
+        if (format_ != Format::kJSON) {
+            throw std::runtime_error("Invalid format!");
+        }
+        return "{ \"data\": \"" + data_ + "\"}";
+    }
+
+private:
+    std::string data_;
+    Format format_;
+};
---
+++ main.cpp
@@ -0,0 +1,44 @@
+﻿#include "SecondaryFunction.h"
+#include "drysolid.hpp"
+
+
+void saveTo(std::ofstream &file, const Printable& printable, Data::Format format)
+{
+    switch (format)
+    {
+    case Data::Format::kText:
+        file << printable.printAsText();
+        break;
+    case Data::Format::kJSON:
+        file << printable.printAsJSON();
+        break;
+    case Data::Format::kHTML:
+        file << printable.printAsHTML();
+        break;
+    }
+}
+
+void saveToAsHTML(std::ofstream &file, const Printable& printable)
+{
+    saveTo(file, printable, Data::Format::kHTML);
+}
+
+void saveToAsJSON(std::ofstream &file, const Printable& printable)
+{
+    saveTo(file, printable, Data::Format::kJSON);
+}
+
+void saveToAsText(std::ofstream &file, const Printable& printable)
+{
+    saveTo(file, printable, Data::Format::kText);
+}
+
+int main(int argc, char** argv)
+{
+	printHeader(L"Принципы DRY и SOLID");
+    
+	
+	
+	std::wcout << "\n";
+	return 0;
+}
